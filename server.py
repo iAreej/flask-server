39c2afe949a0a26f4e851ef6e46287c2330f7bed
Areej Fatimah <areejfatimah125@gmail.com>
2025-01-27 22:50:19 +0500

    Merge remote-tracking branch 'origin/main'

--- server.py
+++ server.py
@@ -68,4 +68,8 @@ def predict():
 
    
 if __name__ == "__main__":
+<<<<<<< HEAD
      app.run(debug=True ,host="0.0.0.0", port=5000)
+=======
+    app.run(debug=True ,host="0.0.0.0", port=5000)
+>>>>>>> 5eb8aa6e (your commit message)
